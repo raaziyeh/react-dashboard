377907c9ed9d71470815c075d89aa8a24f2762cc
raziyeh <116181564+raaziyeh@users.noreply.github.com>
2022-11-27 15:15:24 +0330

    Add Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,8 @@ import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined"
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined"
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined"
-import ListOutlinedIcon from "@mui/icons-material/ListOutlined"
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined"
+
 import { useContext } from "react"
 import { DarkModeContext } from "../../context/darkMode"
 import "./navbar.scss"
@@ -14,50 +15,66 @@ const Navbar = () => {
 	const { darkMode, dispatch } = useContext(DarkModeContext)
 	return (
 		<div className="navbar">
+			<div className="more">
+				<MoreVertOutlinedIcon className="more-icon" />
+			</div>
 			<div className="wrapper">
 				<div className="search">
-					<input type="text" placeholder="Search..." />
+					<input type="text" placeholder="Search..." className="search-input" />
 					<SearchOutlinedIcon />
 				</div>
 				<div className="items">
-					<div className="item">
-						<LanguageOutlinedIcon className="icon" />
-						English
+					<div className="lang">
+						<div className="item">
+							<LanguageOutlinedIcon className="icon" />
+							<span className="lang-choice">English</span>
+						</div>
 					</div>
-					<div className="item">
-						{!darkMode && (
-							<DarkModeOutlinedIcon
-								className="icon"
-								onClick={() => dispatch({ type: "toggle" })}
-							/>
-						)}
-						{darkMode && (
-							<LightModeOutlinedIcon
-								className="icon"
-								onClick={() => dispatch({ type: "toggle" })}
-							/>
-						)}
+					<div className="darkmode-choice">
+						<div className="item">
+							{!darkMode && (
+								<DarkModeOutlinedIcon
+									className="icon"
+									onClick={() => dispatch({ type: "toggle" })}
+								/>
+							)}
+							{darkMode && (
+								<LightModeOutlinedIcon
+									className="icon"
+									onClick={() => dispatch({ type: "toggle" })}
+								/>
+							)}
+						</div>
 					</div>
-					<div className="item">
-						<FullscreenExitOutlinedIcon className="icon" />
+					<div className="fullscreen">
+						<div className="item">
+							<FullscreenExitOutlinedIcon className="icon" />
+						</div>
 					</div>
-					<div className="item">
+					<div className="mini-search">
+						<input
+							type="text"
+							placeholder="Search..."
+							className="search-input"
+						/>
+						<SearchOutlinedIcon className="icon" />
+					</div>
+					<div className="item notif">
 						<NotificationsNoneOutlinedIcon className="icon" />
 						<div className="counter">3</div>
 					</div>
-					<div className="item">
+					<div className="item notif">
 						<ChatBubbleOutlineOutlinedIcon className="icon" />
 						<div className="counter">2</div>
 					</div>
-					<div className="item">
-						<ListOutlinedIcon className="icon" />
-					</div>
-					<div className="item">
-						<img
-							src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-							alt=""
-							className="avatar"
-						/>
+					<div className="avatar-item">
+						<div className="item">
+							<img
+								src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+								alt=""
+								className="avatar"
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,95 +10,112 @@ import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined"
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined"
 import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined"
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined"
 import { Link } from "react-router-dom"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { DarkModeContext } from "../../context/darkMode"
 import "./sidebar.scss"
 
 const Sidebar = () => {
 	const { dispatch } = useContext(DarkModeContext)
+	const [showSidebar, setShowSidebar] = useState(true)
+
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 800) {
+			setShowSidebar(true)
+		} else {
+			setShowSidebar(false)
+		}
+	})
+
 	return (
 		<div className="sidebar">
 			<div className="logo-wrapper">
+				<MoreVertOutlinedIcon
+					className="more-icon"
+					onClick={() => setShowSidebar((prev) => !prev)}
+				/>
 				<Link to="/">
 					<span className="logo">LOGO</span>
 				</Link>
 			</div>
 			<hr />
-			<div className="sidebar-nav">
-				<ul>
-					<p className="title">MAIN</p>
-					<Link to="/">
-						<li>
-							<GridViewOutlinedIcon className="icon" />
-							<span>Dashboard</span>
-						</li>
-					</Link>
+			{showSidebar && (
+				<div className="sidebar-nav">
+					<ul>
+						<p className="title">MAIN</p>
+						<Link to="/">
+							<li>
+								<GridViewOutlinedIcon className="icon" />
+								<span className="action-title">Dashboard</span>
+							</li>
+						</Link>
 
-					<p className="title">LISTS</p>
-					<li>
-						<Link to="/users">
-							<GroupOutlinedIcon className="icon" />
-							<span>Users</span>
-						</Link>
-					</li>
-					<li>
-						<Link to="/products">
-							<Inventory2OutlinedIcon className="icon" />
-							<span>Products</span>
-						</Link>
-					</li>
-					<li>
-						<ViewStreamOutlinedIcon className="icon" />
-						<span>Orders</span>
-					</li>
-					<li>
-						<LocalShippingOutlinedIcon className="icon" />
-						<span>Delivery</span>
-					</li>
-					<p className="title">USEFUL</p>
-					<li>
-						<QueryStatsOutlinedIcon className="icon" />
-						<span>Stats</span>
-					</li>
-					<li>
-						<NotificationsOutlinedIcon className="icon" />
-						<span>Notifications</span>
-					</li>
-					<p className="title">SERVICE</p>
-					<li>
-						<MonitorHeartOutlinedIcon className="icon" />
-						<span>System Health</span>
-					</li>
-					<li>
-						<PsychologyOutlinedIcon className="icon" />
-						<span>Logs</span>
-					</li>
-					<li>
-						<SettingsOutlinedIcon className="icon" />
-						<span>Settings</span>
-					</li>
-					<p className="title">USER</p>
-					<li>
-						<AccountCircleOutlinedIcon className="icon" />
-						<span>Profile</span>
-					</li>
-					<li>
-						<ExitToAppOutlinedIcon className="icon" />
-						<span>Logout</span>
-					</li>
-				</ul>
-				<div className="bottom-of-center">
-					<div
-						className="color-option"
-						onClick={() => dispatch({ type: "light" })}
-					></div>
-					<div
-						className="color-option"
-						onClick={() => dispatch({ type: "dark" })}
-					></div>
+						<p className="title">LISTS</p>
+						<li>
+							<Link to="/users">
+								<GroupOutlinedIcon className="icon" />
+								<span className="action-title">Users</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/products">
+								<Inventory2OutlinedIcon className="icon" />
+								<span className="action-title">Products</span>
+							</Link>
+						</li>
+						<li>
+							<ViewStreamOutlinedIcon className="icon" />
+							<span className="action-title">Orders</span>
+						</li>
+						<li>
+							<LocalShippingOutlinedIcon className="icon" />
+							<span className="action-title">Delivery</span>
+						</li>
+						<p className="title">USEFUL</p>
+						<li>
+							<QueryStatsOutlinedIcon className="icon" />
+							<span className="action-title">Stats</span>
+						</li>
+						<li>
+							<NotificationsOutlinedIcon className="icon" />
+							<span className="action-title">Notifications</span>
+						</li>
+						<p className="title">SERVICE</p>
+						<li>
+							<MonitorHeartOutlinedIcon className="icon" />
+							<span className="action-title">System Health</span>
+						</li>
+						<li>
+							<PsychologyOutlinedIcon className="icon" />
+							<span className="action-title">Logs</span>
+						</li>
+						<li>
+							<SettingsOutlinedIcon className="icon" />
+							<span className="action-title">Settings</span>
+						</li>
+						<p className="title">USER</p>
+						<li>
+							<AccountCircleOutlinedIcon className="icon" />
+							<span className="action-title">Profile</span>
+						</li>
+						<li>
+							<ExitToAppOutlinedIcon className="icon" />
+							<span className="action-title">Logout</span>
+						</li>
+					</ul>
+					<div className="bottom-of-center">
+						<div
+							className="color-option"
+							onClick={() => dispatch({ type: "light" })}
+						></div>
+						<div
+							className="color-option"
+							onClick={() => dispatch({ type: "dark" })}
+						></div>
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	)
 }
